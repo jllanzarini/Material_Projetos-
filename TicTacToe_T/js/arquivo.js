7a51aa3bea2105f508c55272a2293be47413511a
joseluizlanzarini <joseluizlanzarini@gmail.com>
2022-09-22 16:23:21 -0300

    .

--- TicTacToe_T/js/arquivo.js
+++ TicTacToe_T/js/arquivo.js
@@ -1,22 +1,27 @@
 const statusDisplay = document.querySelector('.game--status');
+
 let gameActive = true;
-let currentPlayer = 'X';
-let gameState = ["", "", "", "", "", "", "", ""];
-const winningMEnsage=()=> `Jogador ${curretPlayer} Venceu!`;
-const curretPlayerTurn=()=> `Vez de ${curretPlayer}`;
+let currentPlayer = 'X'
+let gameState = ["","","","","","","","",""];
 
-statusDisplay.innerHTML = curretPlayerTurn();
+const winningMessage=()=> `Jogador ${currentPlayer} Venceu!`;
+const drawMessage = () =>  `Jogo sem Vencedor!`
+const currentPlayerTurn = () => ` Vez de: ${currentPlayer}`;
 
-const winningConditions =[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+statusDisplay.innerHTML = currentPlayerTurn();
+
+const winningConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 function handleCellPlayed(clickedCell, clickedCellIndex){
     gameState[clickedCellIndex] = currentPlayer;
     clickedCell.innerHTML = currentPlayer;
 }
+
 function handlePlayerChange(){
     currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = curretPlayerTurn();
+    statusDisplay.innerHTML = currentPlayerTurn();
 }
+
 function handleResultValidation(){
     let roundWon = false;
     for (let i = 0; i <=7; i++){
@@ -45,4 +50,27 @@ function handleResultValidation(){
         return
     }
     handlePlayerChange();
+
 }
+    function handleCellClick(clickedCellEvent) {
+        const clickedCell = clickedCellEvent.target;
+        const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
+            return;
+        }
+
+        handleCellPlayed(clickedCell, clickedCellIndex);
+        handleResultValidation();
+    }
+    
+    function handleRestartGame(){
+        gameActive = true;
+        currentPlayer = "X";
+        gameState = ["","","","","","","","",""];
+        statusDisplay.innerHTML = currentPlayerTurn();
+        document.querySelectorAll('.cell').forEach(cell=> cell.innerHTML="");
+
+    } 
+    document.querySelectorAll('.cell').forEach(cell=> cell.addEventListener('click', handleCellClick));
+    document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
