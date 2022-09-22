42b6d6e0d19a1508b9a317064272f2dd09cae67a
joseluizlanzarini <joseluizlanzarini@gmail.com>
2022-09-22 16:11:34 -0300

    .

--- TicTacToe_T/js/arquivo.js
+++ TicTacToe_T/js/arquivo.js
@@ -1,6 +1,6 @@
 const statusDisplay = document.querySelector('.game--status');
 let gameActive = true;
-let curretPlayer = 'X';
+let currentPlayer = 'X';
 let gameState = ["", "", "", "", "", "", "", ""];
 const winningMEnsage=()=> `Jogador ${curretPlayer} Venceu!`;
 const curretPlayerTurn=()=> `Vez de ${curretPlayer}`;
@@ -8,3 +8,41 @@ const curretPlayerTurn=()=> `Vez de ${curretPlayer}`;
 statusDisplay.innerHTML = curretPlayerTurn();
 
 const winningConditions =[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+function handleCellPlayed(clickedCell, clickedCellIndex){
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+function handlePlayerChange(){
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = curretPlayerTurn();
+}
+function handleResultValidation(){
+    let roundWon = false;
+    for (let i = 0; i <=7; i++){
+        const winningCondition = winningConditions[i];
+        let a = gameState[winningCondition[0]];
+        let b = gameState[winningCondition[1]];
+        let c = gameState[winningCondition[2]];
+
+        if (a === '' ||b === '' ||c === '' ){
+            continue;
+        }
+        if(a ===b && b===c){
+            roundWon = true;
+            break
+        }
+    }
+    if(roundWon){
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+    let roundDraw = !gameState.includes("");
+    if(roundDraw){
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return
+    }
+    handlePlayerChange();
+}
