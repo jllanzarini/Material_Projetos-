9bfa524ebb6fbad69b9bd9cee57ec7ef54b39d43
joseluizlanzarini <joseluizlanzarini@gmail.com>
2022-10-25 11:03:39 -0300

    .

---
+++ Projeto_Manhã/Fonte/projetoweb_m/src/test/java/com/example/projetoweb_m/ProjetowebMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoweb_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
