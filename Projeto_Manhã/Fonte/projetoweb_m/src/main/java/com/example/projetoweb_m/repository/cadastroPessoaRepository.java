3a2c94c34a3cc38acce8cf77fe91485ec09add61
joseluizlanzarini <joseluizlanzarini@gmail.com>
2022-10-31 10:57:48 -0300

    .
    .

--- Projeto_Manhã/Fonte/projetoweb_m/src/main/java/com/example/projetoweb_m/repository/cadastroPessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projetoweb_m.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.projetoweb_m.model.cadastroPessoa;
-@Repository
-public interface cadastroPessoaRepository  extends JpaRepository<cadastroPessoa, Integer>{
-
-}
