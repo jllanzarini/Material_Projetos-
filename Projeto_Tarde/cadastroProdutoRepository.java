3e6f087fb0ffcae45c0c27129e92158586ce0716
jllanzarini <114111905+jllanzarini@users.noreply.github.com>
2022-11-08 14:21:23 -0300

    Add files via upload

---
+++ Projeto_Tarde/cadastroProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.example.projetoweb_t.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.projetoweb_t.model.cadastroProduto;
+
+public interface cadastroProdutoRepository extends JpaRepository<cadastroProduto, Integer> {
+
+}
