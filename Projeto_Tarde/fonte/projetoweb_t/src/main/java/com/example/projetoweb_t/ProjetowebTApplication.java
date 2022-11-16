3ecfef19e0beddfdaafcb518ae9ad1ac5f54cc98
joseluizlanzarini <joseluizlanzarini@gmail.com>
2022-11-16 08:55:13 -0300

    .

--- Projeto_Tarde/fonte/projetoweb_t/src/main/java/com/example/projetoweb_t/ProjetowebTApplication.java
+++ Projeto_Tarde/fonte/projetoweb_t/src/main/java/com/example/projetoweb_t/ProjetowebTApplication.java
@@ -2,8 +2,14 @@ package com.example.projetoweb_t;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.example.projetoweb_t.model"})
+@EnableJpaRepositories(basePackages = {"com.example.projetoweb_t.repository"})
+@ComponentScan(basePackages = {"com.example.projetoweb_t.controller"})
 public class ProjetowebTApplication {
 
 	public static void main(String[] args) {
