13a297b10a50a0efc0d296ca999b64797c5750b5
joseluizlanzarini <joseluizlanzarini@gmail.com>
2022-11-25 16:17:42 -0300

    .

--- Projeto_Tarde/fonte/sistemaweb_tarde/src/main/java/com/example/controle/PessoasController.java
+++ Projeto_Tarde/fonte/sistemaweb_tarde/src/main/java/com/example/controle/PessoasController.java
@@ -1,23 +1,28 @@
 package com.example.controle;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.web.repositorio.PessoaRepositorio;
 
+
+
 @Controller
 public class PessoasController {
 	
-	@Autowired
-	private PessoaRepositorio pessoarepository;
+	private PessoaRepositorio pessoarepositorio;
+	
+	
+	public PessoasController(PessoaRepositorio pessoarepositorio ) {
+		this.pessoarepositorio =pessoarepositorio;
+	}
+	
 	
 	//Listagem das Pessoas Cadastradas
-	@GetMapping("/cadastro/pessoas")
+	@GetMapping("/pessoas")
 	public String pessoas(Model model) {
-		model.addAttribute("listaPessoas",pessoarepository.findAll());
+		model.addAttribute("listaPessoas",pessoarepositorio.findAll());
 		return "cadastros/pessoa";
 	}
-
 }
