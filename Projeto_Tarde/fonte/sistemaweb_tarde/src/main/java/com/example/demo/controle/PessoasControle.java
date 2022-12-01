eacb3f3b2c3f3a52906e26c4d76afb5f971b159b
jllanzarini <114111905+jllanzarini@users.noreply.github.com>
2022-12-01 15:21:59 -0300

    .

--- Projeto_Tarde/fonte/sistemaweb_tarde/src/main/java/com/example/demo/controle/PessoasControle.java
+++ Projeto_Tarde/fonte/sistemaweb_tarde/src/main/java/com/example/demo/controle/PessoasControle.java
@@ -1,16 +1,19 @@
 package com.example.demo.controle;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.modelo.Pessoas;
 import com.example.demo.repositorio.PessoasRepositorio;
 
-import jakarta.validation.Valid;
 
 @Controller
 public class PessoasControle {
@@ -25,30 +28,36 @@ private PessoasRepositorio pessoasrepositorio;
 	public String Inicial() {
 		return "index";
 	}
-	
-		
-	//Listagem das Pessoas Cadastradas
-	@GetMapping("/pessoas")
-	public String pessoas(Model model) {
-		model.addAttribute("listaPessoas",pessoasrepositorio.findAll());
-		return "cadastros/lista_pessoa";
-	}
-	
-	//Cadastrar Pessoa
-		@GetMapping("/pessoas/nova")
-		public String novaPessoa(Model model) {
-			
-			model.addAttribute("pessoas", new Pessoas());
-			return "cadastros/form_pessoa";
-
+	//Lista todas as Pessoas cadastradas
+	  @GetMapping("/pessoas")
+	  public String listar(ModelMap model) {
+		  model.addAttribute("listaPessoas", pessoasrepositorio.findAll());
+		  return "/cadastros/lista_pessoas";
+	  }
+	  //instancia uma nova pessoa e chamar a tela form_pessoaa
+	 @GetMapping("/pessoas/nova")
+		  public String cadastrar(@ModelAttribute("pessoas") Pessoas pessoas) {
+			  return "/cadastros/form_pessoa";//forwoard
+		  }
+	 //Guarda as informações no BD
+	 @PostMapping("/pessoas/salvar")
+	  public String salvar(@ModelAttribute("pessoas") Pessoas pessoas, RedirectAttributes attr) {
+		  pessoasrepositorio.save(pessoas);
+		  attr.addFlashAttribute("success", "Pessoa adicionada com sucesso");
+		  return "redirect:/pessoas";//redirect
+	  }
+	 
+	 //Excluir os registro de pessoas
+	  @GetMapping("/pessoas/excluir/{id}")
+		public String excluirPessoa(@PathVariable("id") long id) {
+			Optional<Pessoas> pessoaOpt = pessoasrepositorio.findById(id);
+			if (pessoaOpt.isEmpty()) {
+				throw new IllegalArgumentException("Pessoa inválida.");
+			}
+		pessoasrepositorio.delete(pessoaOpt.get());
+			return "redirect:/pessoas";
 		}
-		
-		@PostMapping("/pessoas/salvar")
-		public String salvarPessoa(@Valid @ModelAttribute("pessoas") Pessoas pessoas, BindingResult bindingResult, Model model) {
-				return "pessoas/novapessoa";
-			
-		}
-	
-	
+	  
+	  
 	
 }
